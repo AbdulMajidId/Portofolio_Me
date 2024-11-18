c32ace5448c2410dcdfdafba341edc833de3e4cf
Abdul Majid <109328635+AbdulMajidId@users.noreply.github.com>
2024-11-18 16:25:19 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const nav = document.getElementById('nav');
 window.addEventListener('scroll', function(){
     scrollposition = this.window.scrollY;
 
-    if (scrollposition >=10 ){
+    if (scrollposition >=0 ){
         nav.classList.add('nav-dark');
-    } else if (scrollposition <=10) {
+    } else if (scrollposition <=0) {
         nav.classList.remove('nav-dark');
     }
 })
